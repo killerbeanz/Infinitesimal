3c8d0084d95520be65354405c44d29d655c2f4c4
killerbeanz <ian@thesolloways.com>
2025-05-19 23:37:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -195,7 +195,7 @@ antiholeUpgrades.squareLimit.button.addEventListener('click', buyAntiholeUpgrade
 
 function triggerHueShift() {
   hueShifts++;
-  softcapPower = softcapPower.pow(.5);
+  softcapPower = softcapPower.mul(.5);
   resetGameProgress();
   hueShiftModal.style.display = 'none';
   showHueShiftPrompt = false;
